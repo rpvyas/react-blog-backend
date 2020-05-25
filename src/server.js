9aa821917e462666951cb3c9397bba614e842d09
Ravi <vyas.ravi.1991@gmail.com>
2020-05-25 15:15:29 -0700

    added post

--- src/server.js
+++ src/server.js
@@ -1,9 +1,23 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
+// use bodyparser middleware for Post requests
+app.use(bodyParser.json());
+
 app.get('/hello', (req, res) => {
     res.send('Hello!');
+});
+
+app.get('/hello/:name', (req, res) => {
+    const name = req.params.name;
+    res.send(`Hello ${name}`);
+});
+
+app.post('/hello', (req, res) => {
+    const name = req.body.name;
+    res.send(`Hello ${name}!!!!`);
 });
 
 app.listen(8000, () => {
